4ee11338561a44e328cbd0eaf40de23be31d0cb2
sadhin28 <taosifsadhin113@gmail.com>
2025-10-21 01:15:06 +0600

    server create successfully

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 const app = express()
+app.use(express.json({ limit: '50mb' }));
 require('dotenv').config()
 app.use(cors());
 const { MongoClient, ServerApiVersion } = require('mongodb');
